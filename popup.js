f9112e08265391d5303c0ed629b0d1e0c4412baa
yoyowarmli <yoyowarmli@jetfuel.com.tw>
2019-07-22 16:21:28 +0800

    v.1.0

--- popup.js
+++ popup.js
@@ -1,15 +1,11 @@
 window.addEventListener("load", function() {
-  console.log("POPUP: OnLoad");
-  var link = document.getElementById('res');
+  console.log("POPUP: OnLoad")
+  reload()
+  var link = document.getElementById('res')
   link.addEventListener('click', function() {
     reload()
-  });
-}, false);
-var bg = chrome.extension.getBackgroundPage();
-function getBG() {
-  return chrome.extension.getBackgroundPage();
-}
-
+  })
+}, false)
 
 function generateBtn (items) {
   items = JSON.parse(items)
@@ -18,16 +14,16 @@ function generateBtn (items) {
   let buttons = []
   let noData = document.getElementById("nodata");
   if (buttons.length > 0) { noData.innerHTML = '目前沒有資料'} else { noData.innerHTML = ''}
-  medias.map( media => {
+  medias.forEach( media => {
     let data = {}
     data.title = media.title
     data.url = media.files[0].subtitles.url
     buttons.push(data)
   }) 
   
-  buttons.map( button => {
-    let currentDiv = document.getElementById("container");
-    let newText = document.createTextNode(`${button.title}`); 
+  buttons.forEach( button => {
+    let currentDiv = document.getElementById("container")
+    let newText = document.createTextNode(`${button.title}`)
     let newDiv = document.createElement('div')
     let newLink = document.createElement('a')
     let newIcon = document.createElement('img')
@@ -36,60 +32,56 @@ function generateBtn (items) {
     newLink.appendChild(newText)
     newDiv.appendChild(newIcon)
     newLink.setAttribute("data-url", button.url)
-    // newLink.setAttribute("target", '_blank')
     newLink.onclick =function(newLink) {
       download(newLink)
     }
     newDiv.setAttribute("class", 'column')
     newDiv.appendChild(newLink)
     currentDiv.appendChild(newDiv)
-    // document.body.insertBefore(newDiv, currentDiv); 
   })
-    
 }
-var currentTab;
-var version = "1.0";
-var query = { active: true, currentWindow: true };
+
+let currentTab;
+let version = "1.0"
+let query = { active: true, currentWindow: true }
 function reload () {
   chrome.tabs.query(
     query,
     function(tabArray) {
-      chrome.tabs.update(tabArray[0].id, {url: tabArray[0].url});
-        currentTab = tabArray[0];
+      chrome.tabs.update(tabArray[0].id, {url: tabArray[0].url})
+        currentTab = tabArray[0]
         chrome.debugger.attach({
             tabId: currentTab.id
-        }, version, onAttach.bind(null, currentTab.id));
+        }, version, onAttach.bind(null, currentTab.id))
     }
-)
+  )
 }
 
-reload()
+
 function download (e) {
   let url = e.path[0].dataset['url']
-  var request = new XMLHttpRequest();
-  request.open('GET', `${url}`, true);
-
+  let request = new XMLHttpRequest()
+  request.open('GET', `${url}`, true)
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
-    var blob = new Blob([request.responseText], {type: "text/plain;charset=utf-8"});
+    let blob = new Blob([request.responseText], {type: "text/plain;charset=utf-8"});
         saveAs(blob, `${e.path[0].innerHTML}.vtt`)
-      // Success!
-
     }
-  };
-  request.send();
+  }
+  request.send()
 }
 function onAttach(tabId) {
   chrome.debugger.sendCommand({
       tabId: tabId
-  }, "Network.enable");
+  }, "Network.enable")
 
   chrome.debugger.onEvent.addListener(allEventHandler);
 }
 
 function allEventHandler(debuggeeId, message, params) {
+  console.log({debuggeeId, message, params})
   if (currentTab.id != debuggeeId.tabId) {
-      return;
+      return
   }
 
   if (message == "Network.responseReceived") {
@@ -104,6 +96,6 @@ function allEventHandler(debuggeeId, message, params) {
         }
       } catch {
       }
-    });
+    })
   }
 }
